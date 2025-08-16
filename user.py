eb40c8bb7ec582adc35485ca4eea1568c10aa7a1
kamaldhitalofficial <kamaldhitalofficial@gmail.com>
2025-08-16 22:31:41 +0545

    Added creation_time to user information

--- user.py
+++ user.py
@@ -1,5 +1,13 @@
+from datetime import datetime
+
 class User:
-    def __init__(username, name, email):
+    def __init__(self, username, name, email):
         self.username = username
         self.name = name
         self.email = email
+        self.creation_time = User.creation_time()
+
+    def creation_time():
+        current_datetime = datetime.now()
+        formatted_time = current_datetime.strftime("%Y/%m/%d %H:%M:%S")
+        return formatted_time
