9ad95b689f50df8a672b56f033cb81e86f5ca0b2
kamaldhitalofficial <kamaldhitalofficial@gmail.com>
2025-08-16 22:33:16 +0545

    Auto generated unique user id for each user

--- user.py
+++ user.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import uuid
+
 
 class User:
     def __init__(self, username, name, email):
@@ -6,8 +8,13 @@ class User:
         self.name = name
         self.email = email
         self.creation_time = User.creation_time()
+        self.user_id = User.unique_id()
 
     def creation_time():
         current_datetime = datetime.now()
         formatted_time = current_datetime.strftime("%Y/%m/%d %H:%M:%S")
         return formatted_time
+
+    def unique_id():
+        user_id = str(uuid.uuid4())
+        return user_id
