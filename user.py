7ae75e515ae33345f9bcf7759fc88a7e3faa9323
kamaldhitalofficial <kamaldhitalofficial@gmail.com>
2025-08-18 23:36:58 +0545

    Validate username to make it unique across the system

--- user.py
+++ user.py
@@ -3,14 +3,21 @@ import uuid
 
 
 class User:
+    _usernames = set()
+
     def __init__(self, username, name, email, bio, profile_picture_url):
+        if username in User._usernames:
+            raise ValueError(f"Username '{username}' is already taken.")
+        else:
+            self.username = username
         self.user_id = User.unique_id()
-        self.username = username
         self.name = name
         self.email = email
         self.creation_time = User.creation_time()
         self.bio = bio
         self.profile_picture_url = profile_picture_url
+
+        User._usernames.add(username)
 
     def creation_time():
         current_datetime = datetime.now()
@@ -22,7 +29,11 @@ class User:
         return user_id
 
     def update_profile(self, username=None, name=None, email=None, bio=None, profile_picture_url=None):
-        if username:
+        if username and username != self.username:
+            if username in User._usernames:
+                raise ValueError(f"Username '{username}' is already taken.")
+            User._usernames.remove(self.username)
+            User._usernames.add(username)
             self.username = username
 
         if name:
@@ -34,6 +45,7 @@ class User:
         if profile_picture_url:
             self.profile_picture_url = profile_picture_url
 
+
     def get_profile_info(self):
         return f"""
 User ID: {self.user_id}
@@ -44,6 +56,7 @@ Bio: {self.bio}
 Profile Picture URL: {self.profile_picture_url}
 Account Created: {self.creation_time}
 """
+
 
     def __str__(self):
         return self.get_profile_info()
