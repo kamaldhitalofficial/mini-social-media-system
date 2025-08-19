c8aece9b5b02950593b281243ccf6a21c8f8b8b3
kamaldhitalofficial <kamaldhitalofficial@gmail.com>
2025-08-19 21:30:48 +0545

    Added post class with private attributes like post_id, content, author and timestamp

---
+++ post.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+import uuid
+
+
+class Post:
+
+    def __init__(self, author, content):
+        self._post_id = str(uuid.uuid4())
+        self.author = author
+        self.content = content
+        self._timestamp = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+
+
+        def get_post_id(self):
+            return self._post_
+
+
+        def get_author(self):
+            return self._author
+
+
+        def get_content(self):
+            return self._content
+
+
+        def get_timestamp(self):
+            return self._timestamp
