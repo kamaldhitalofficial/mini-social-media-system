483c7c28b489a872c80b6c61af1138d67febe765
kamaldhitalofficial <kamaldhitalofficial@gmail.com>
2025-08-19 22:59:38 +0545

    Fix errors and added str and repr methods

--- post.py
+++ post.py
@@ -4,35 +4,49 @@ import uuid
 
 class Post:
 
-    def __init__(self, author, content):
-        self._post_id = str(uuid.uuid4())
-        self.author = author
-        self.content = content
-        self._timestamp = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-        self._like_count = 0
-        self._view_count = 0
+    def __init__(self, content, author):
+        self.__post_id = str(uuid.uuid4())
+        self.__author = author
+        self.__content = content
+        self.__timestamp = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        self.__like_count = 0
+        self.__view_count = 0
 
 
-        def get_post_id(self):
-            return self._post_
+    def get_post_id(self):
+        return self.__post_id
 
 
-        def get_author(self):
-            return self._author
+    def get_author(self):
+        return self.__author
 
 
-        def get_content(self):
-            self._view_count += 1
-            return self._content
+    def get_content(self):
+        self.__view_count += 1
+        return self.__content
 
 
-        def get_timestamp(self):
-            return self._timestamp
+    def get_timestamp(self):
+        return self.__timestamp
 
 
-        def like_post(self):
-            self._like_count += 1
+    def like_post(self):
+        self.__like_count += 1
 
 
-        def get_likes(self):
-            return self.like_count
+    def get_likes(self):
+        return self.__like_count
+
+
+    def get_views(self):
+        return self.__view_count
+
+
+    def __str__(self):
+        return (f"Post by @{self.__author.username} at {self.__timestamp}:\n"
+                f"{self.__content}\n"
+                f"❤️ {self.__like_count} | 👁️ {self.__view_count}")
+
+    def __repr__(self):
+        return (f"Post(post_id='{self.__post_id}', author='{self.__author.username}', "
+                f"likes={self.__like_count}, views={self.__view_count})")
